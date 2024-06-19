2186426189d396ff48b4e53172cf0af9808984d5
zadkayif <alzaribedjo@gmail.com>
2024-06-19 08:59:25 +0700

    new commit

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,44 @@
 <?php
 
-// use App\Http\Controllers\TodoController;
-
+use App\Http\Controllers\TodoController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\PesananController;
-use App\Http\Controllers\TodoController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/todo', function () {
-//     return view("todo.app");
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/todo', [TodoController::class, 'index'])->name('todo');
-Route::post('/todo', [TodoController::class, 'store'])->name('todo.post');
-Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update');
-Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.delete');
+Route::get('/todo', function () {
+    return view("todo.app");
+});
 
-Route::get('/pesanan', [PesananController::class, 'index'])->name('pesanan');
+Route::middleware('auth')->group(function () {
 
-Route::get('/menu', [MenuController::class, 'index'])->name('Menu');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/todo', [TodoController::class, 'index'])->name('todo');
+    Route::post('/todo', [TodoController::class, 'store'])->name('todo.post');
+    Route::put('/todo/{id}', [TodoController::class, 'update'])->name('todo.update');
+    Route::delete('/todo/{id}', [TodoController::class, 'destroy'])->name('todo.delete');
+
+    Route::get('/pesanan', [PesananController::class, 'index'])->name('pesanan');
+
+    Route::get('/menu', [MenuController::class, 'index'])->name('menu');
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/dashboard', [HomeController::class, 'dashboard'])->name('admin.dashboard');
+    Route::get('/admin/products', [ProductController::class, 'index'])->name('admin.products');
+});
+
+require __DIR__ . '/auth.php';
